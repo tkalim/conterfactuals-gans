c54820f0b6a773c433f889b69654cab8e2838d2c
tariq <t.kalim.95@gmail.com>
2020-11-19 19:06:38 +0100

    dataset for smiling classifier

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -108,3 +108,80 @@ class CelebADataset(CelebA):
             return {"image": X, "filename": self.filename[index], "target": target}
         elif self.lone_attr is not None & self.attr[index, self.lone_attr_idx] == 1:
             return {"image": X, "filename": self.filename[index], "target": target}
+
+
+class SmilingNotSmilingCelebADataset(CelebA):
+    """
+    slightly modified CelebA dataset to return smiling and
+    not smiling images
+    """
+
+    def __init__(
+        self,
+        root: str,
+        split: str = "train",
+        target_type: Union[List[str], str] = "attr",
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        download: bool = False,
+    ) -> None:
+        super(self.__class__, self).__init__(
+            root,
+            split,
+            target_type,
+            transform,
+            target_transform,
+            download,
+        )
+        self.positive_class = "Smiling"
+
+        attributes_list = list(
+            pd.read_csv(Path(self.root) / "list_attr_celeba.txt").columns
+        )
+        self.positive_class_idx = attributes_list.index(self.lone_attr)
+
+    def __getitem__(self, index):
+        X = PIL.Image.open(
+            os.path.join(
+                self.root, self.base_folder, "img_align_celeba", self.filename[index]
+            )
+        )
+
+        target: Any = []
+        for t in self.target_type:
+            if t == "attr":
+                target.append(self.attr[index, :])
+            elif t == "identity":
+                target.append(self.identity[index, 0])
+            elif t == "bbox":
+                target.append(self.bbox[index, :])
+            elif t == "landmarks":
+                target.append(self.landmarks_align[index, :])
+            else:
+                raise ValueError('Target type "{}" is not recognized.'.format(t))
+
+        if self.transform is not None:
+            X = self.transform(X)
+
+        if target:
+            target = tuple(target) if len(target) > 1 else target[0]
+
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+        else:
+            target = None
+
+        if self.attr[index, self.positive_class_idx] == 1:
+            return {
+                "image": X,
+                "filename": self.filename[index],
+                "label_name": "Smiling",
+                "label": 1,
+            }
+        elif self.attr[index, self.lone_attr_idx] == -1:
+            return {
+                "image": X,
+                "filename": self.filename[index],
+                "label_name": "Not Smiling",
+                "label": 0,
+            }
