d82ab8149340866522bec9e3c882b00e283e7c12
tariq <t.kalim.95@gmail.com>
2020-11-19 15:42:08 +0100

    save latent vectors

--- src/generate_activations.py
+++ src/generate_activations.py
@@ -1,17 +1,22 @@
+from pathlib import Path
+
+import numpy as np
 import torch
 import torchvision
-from pathlib import Path
+
 from .data.datasets import CelebADataset
+from .models.vanilla_vae import VanillaVAE
 
 CELEBA_DIR = "./data/celeba"
 BATCH_SIZE = 20
 ACTIATIONS_DIR = "./data/activations"
+MODEL_STATE_DICT = "../models/AE.pt"
 use_gpu = True if torch.cuda.is_available() else False
 
 
 def predict(model, dataloader, model_weights=None):
     """
-    predicts latent features of an celeba-like image
+    predicts latent features of a celeba-like image
     """
     if model_weights is not None:
         model.load_state_dict(torch.load(model_weights))
@@ -32,12 +37,23 @@ def predict(model, dataloader, model_weights=None):
             batch_images = batch_images.to(device="cuda")
         with torch.no_grad():
             output = model(batch_images)
+            for idx, latent_vector in enumerate(output):
+                filename = Path(batch_filenames[idx]).with_suffix(".npy")
+                filepath = Path(ACTIATIONS_DIR) / filename
+                np.save(filepath, latent_vector)
 
     return None
 
 
 if __name__ == "__main__":
-    dataset = torchvision.datasets.CelebA(
-        root=CELEBA_DIR, split="all", target_type="attr", transform=None, download=True
+    dataset = CelebADataset(
+        root=CELEBA_DIR,
+        split="all",
+        target_type="attr",
+        transform=None,
+        download=True,
+        lone_attr="Smiling",
     )
     dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE)
+    model = VanillaVAE()
+    model.load_state_dict(torch.load(MODEL_STATE_DICT))
